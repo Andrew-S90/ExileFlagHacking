8a376edb3b2febb4de0b13f4ef0631148093683d
Andrew <Andrew-S90@users.noreply.github.com>
2020-10-06 13:09:14 -0500

    FlagHacking

--- Exile.Altis/config.cpp
+++ Exile.Altis/config.cpp
@@ -9,6 +9,77 @@
  * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/.
  */
  
+class CfgFlagHacking
+{
+	enableHacking = 1;
+	failChance = 25;
+	hackDuration = 10;
+	maxHackAttempts = 3;
+	minPlayers = 30;
+	maxHacks = 3;
+	removeChance = 5;
+	showMapIcon = 1;
+	notifyServer = 1;
+	notificationCooldown = 5;
+}; 
+
+class CfgExileDelayedActions
+{
+	class Abstract
+	{
+		duration=10;
+		abortInCombatMode=1;
+		durationFunction="";
+		animation="";
+		animationType="switchMove";
+		failChance=0;
+		conditionFunction="";
+		completedFunction="";
+		abortedFunction="";
+		failedFunction="";
+		repeatingCheckFunction="";
+	};
+	class HackFlag: Abstract
+	{
+		animationType="switchMove";
+		animation="Acts_TerminalOpen";
+		abortInCombatMode=0;
+		failChanceFunction="ExileClient_action_hackFlag_failChance";
+		durationFunction="ExileClient_action_hackFlag_duration";
+		conditionFunction="ExileClient_action_hackFlag_condition";
+		completedFunction="ExileClient_action_hackFlag_completed";
+		failedFunction="ExileClient_action_hackFlag_failed";
+		abortedFunction="ExileClient_action_hackFlag_aborted";
+	};
+};
+ 
+class CfgNetworkMessages
+{
+	class hackFlagRequest
+	{
+		module="object_flag";
+		parameters[]=
+		{
+			"STRING"
+		};
+	};
+	class updateFlagHackAttemptRequest
+	{
+		module="object_flag";
+		parameters[]=
+		{
+			"STRING"
+		};
+	};
+	class startFlagHackRequest
+	{
+		module="object_flag";
+		parameters[]=
+		{
+			"STRING"
+		};
+	};
+};
 
 class CfgClans
 {
@@ -4196,6 +4267,12 @@ class CfgInteractionMenus
 				action = "_this call ExileClient_gui_baseManagement_event_show";
 			};
 			*/
+			class HackFlag: ExileAbstractAction
+			{
+				title = "Hack Flag";
+				condition = "(getNumber(missionConfigFile >> 'CfgFlagHacking' >> 'enableHacking') isEqualTo 1) && ('Exile_Item_Laptop' in (magazines player)) && !ExilePlayerInSafezone";
+				action = "['HackFlag', _this select 0] call ExileClient_action_execute";
+			};
 			class StealFlag: ExileAbstractAction
 			{
 				title = "Steal Flag";
